82a403e42996d5cf01253ce79bafc56a23d46099
Ilya Akinchyts <ilya.akinchyts@gmail.com>
2021-10-28 17:19:09 +0300

    add in AuthorController: GetMapping, PostMapping, PutMapping, DeleteMapping add postgresSQL

--- src/main/java/com/academia/Application.java
+++ src/main/java/com/academia/Application.java
@@ -1,13 +1,13 @@
-package com.academia.book;
+package com.academia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/academia/model/BookApplicationTests.java
+++ src/test/java/com/academia/model/BookApplicationTests.java
@@ -1,4 +1,4 @@
-package com.academia.book;
+package com.academia.model;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
